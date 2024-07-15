1310217f8de45737515a08264da2279d5842b796
Rodrigo Adriano <rdgadriano@gmail.com>
2024-07-15 00:07:02 -0300

    Domingo-SQL-Ok

--- BackEnd/src/models/connection.js
+++ BackEnd/src/models/connection.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2/promise');
-
+require('dotenv').config();
 const HOST =  process.env.SQL_HOST
 const USER =  process.env.SQL_USER
 const PASS =  process.env.SQL_PASS
 const DB =  process.env.SQL_DB
+
+console.log(HOST)
 
 const connection = mysql.createPool({
     host: HOST,
