79a71310f8c418d99f86a294a97bcba008389e24
Rodrigo Adriano <rdgadriano@gmail.com>
2024-08-09 17:14:54 -0300

    AdicionadoMiddelware na ROTA

--- BackEnd/src/router.ts
+++ BackEnd/src/router.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import Taskcontroller from './controllers/Taskcontroller';
+import tksMiddleware from './middlewares/tksMiddleware';
 const router = express.Router();
 
 router.get('/tasks', Taskcontroller.getAll);
-router.post('/tasks', Taskcontroller.createTask);
-router.put('/tasks/:id', Taskcontroller.updateTask);
+router.post('/tasks', tksMiddleware.titleValidade, Taskcontroller.createTask);
+router.put('/tasks/:id',tksMiddleware.statusValidade,tksMiddleware.titleValidade,Taskcontroller.updateTask);
 router.delete('/tasks/:id', Taskcontroller.deleteTask);
 
 export default router;
